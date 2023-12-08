62a519a967f3eda53dfb01a0daaf435b381ba79b
Nestor Garcia <ngarcia@ologistics.com>
2023-12-08 15:17:53 -0600

    pruebas de variables de entorno

--- src/main/java/EPA/Cuenta_Bancaria_Web/drivenAdapters/repositorios/I_RepositorioClienteMongo.java
+++ src/main/java/EPA/Cuenta_Bancaria_Web/drivenAdapters/repositorios/I_RepositorioClienteMongo.java
@@ -1,7 +1,6 @@
 package EPA.Cuenta_Bancaria_Web.drivenAdapters.repositorios;
 
 import EPA.Cuenta_Bancaria_Web.models.Mongo.M_ClienteMongo;
-import EPA.Cuenta_Bancaria_Web.models.Mongo.M_CuentaMongo;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 public interface I_RepositorioClienteMongo extends ReactiveMongoRepository<M_ClienteMongo, String>
